624d3a0fd371953bb27d74da65acc2b7adf4082a
julydeath <manojkarajada.mk@gmail.com>
2023-02-14 17:02:45 +0530

    counter with class state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,22 +4,22 @@ class Counter extends Component {
   constructor() {
     super();
     this.state = {
-      count: 0,
+      counter: 0,
     };
   }
 
   handleclick() {
     this.setState({
-      count: this.state.count + 1,
+      counter: this.state.counter + 1,
     });
   }
 
   render() {
     return (
-      <div>
-        This is Counter {this.state.count}
-        <button onClick={() => this.handleclick()}>increment</button>
-      </div>
+      <>
+        this is counter : {this.state.counter}
+        <button onClick={() => this.handleclick()}>click me</button>
+      </>
     );
   }
 }
