8afc51090fd480135683d0aef532b01560636622
julydeath <manojkarajada.mk@gmail.com>
2023-02-14 14:56:01 +0530

    Chore : basic class and function components added

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Greet() {
+//   return <h1>This is Greet</h1>;
+// }
+
+const Greet = () => {
+  return <h1>This is Functional Component</h1>;
+};
+
+export default Greet;
---
+++ src/components/Hello.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Hello = () => {
+  //   return (
+  //     <div className="dummyclass">
+  //       <h1>Hello manoj</h1>
+  //     </div>
+  //   );
+
+  // without JSX
+
+  return React.createElement(
+    "div",
+    { id: "1", className: "dummyClass" },
+    React.createElement("h1", null, "Hello manoj")
+  );
+};
+
+export default Hello;
